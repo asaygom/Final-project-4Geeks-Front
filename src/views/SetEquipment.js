6b265bd1e79d4a2b602c197745d2e0ea385ab5f2
Alexis Sayago <as.sayagom@gmail.com>
2023-08-07 21:10:49 -0400

    minor changes on equipment form

--- src/views/SetEquipment.js
+++ src/views/SetEquipment.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -8,79 +9,98 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function SetEquipment() {
-  const handleSubmit = (event) => {
+    const [equipmentStatus, setEquipmentStatus] = useState('');
+
+    const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+        equipment_name: data.get('equipment_name'),
+        equipment_description: data.get('equipment_description'),
+        equipment_status: data.get('equipment_status'),
+        equipment_active: data.get('is_active')
     });
-  };
+    };
+
+    const handleChange = (event) => {
+      setEquipmentStatus(event.target.value);
+    };    
 
   return (
-    <Container component="main" maxWidth="xs">
-    <CssBaseline />
-    <Button variant="text" size="small">Back</Button>
-    <Box
-        sx={{
-        marginTop: 5,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        }}
-    >
-        <Typography component="h1" variant="h5">
-        New equipment
-        </Typography>
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-        <Grid container spacing={2}>
-            <Grid item xs={12}>
-            <TextField
-                name="equipment_name"
-                required
-                fullWidth
-                id="equipment_name"
-                label="Name"
-                autoFocus
-            />
-            </Grid>
-            <Grid item xs={12}>
-            <TextField
-                name="equipment_description"
-                required
-                fullWidth
-                id="equipment_description"
-                label="Description"
-            />
-            </Grid>
-            <Grid item xs={12}>
-            <TextField
-                name="equipment_status"
-                required
-                fullWidth
-                id="equipment_status"
-                label="Status"
-            />
-            </Grid>
-            <Grid item xs={12}>
-            <FormControlLabel
-                control={<Checkbox value="is_active" color="primary" />}
-                label="Active"
-            />
-            </Grid>
-        </Grid>
-        <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+    <>
+        <Button variant="text" size="small">Back</Button>
+        <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+            sx={{
+            marginTop: 5,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            }}
         >
-            Apply
-        </Button>
+            <Typography component="h1" variant="h5">
+            New equipment
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                <TextField
+                    name="equipment_name"
+                    required
+                    fullWidth
+                    id="equipment_name"
+                    label="Name"
+                    autoFocus
+                />
+                </Grid>
+                <Grid item xs={12}>
+                <TextField
+                    name="equipment_description"
+                    required
+                    fullWidth
+                    id="equipment_description"
+                    label="Description"
+                />
+                </Grid>
+                <Grid item xs={12}>
+                    <InputLabel id="equipment_status_label">Status</InputLabel>
+                    <Select
+                    name='equipment_status'
+                    labelId="equipment_status_label"
+                    id="equipment_status"
+                    value={equipmentStatus}
+                    label="Status"
+                    onChange={handleChange}
+                    >
+                        <MenuItem value='malfunction'>Malfunction</MenuItem>
+                        <MenuItem value='not_working'>Not working</MenuItem>
+                        <MenuItem value='working'>Working</MenuItem>
+                    </Select>
+                </Grid>
+                <Grid item xs={12}>
+                <FormControlLabel
+                    control={<Checkbox name='is_active' value="is_active" color="primary" />}
+                    label="Active"
+                />
+                </Grid>
+            </Grid>
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+            >
+                Apply
+            </Button>
+            </Box>
         </Box>
-    </Box>
-    </Container>
+        </Container>
+    </>
   );
 }
